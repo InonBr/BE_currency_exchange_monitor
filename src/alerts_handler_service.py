9f1fa850b6fc249b266ec89b24da848176b16f0c
Inon Bercu <inonbr@gmail.com>
2021-06-16 15:40:16 +0300

    post request for webhook url is working

--- src/alerts_handler_service.py
+++ src/alerts_handler_service.py
@@ -1,5 +1,5 @@
 from kafka import KafkaConsumer
-from utils import string_to_list
+from utils import string_to_list, webhook_post_request
 
 consumer = KafkaConsumer('alert_created', bootstrap_servers=['localhost:9092'])
 
@@ -7,5 +7,6 @@ for data in consumer:
     decoded_data = data.value.decode('utf-8')
     decoded_data = string_to_list(decoded_data)
 
-    print(type(decoded_data))
-    print(decoded_data)
+    response = webhook_post_request(decoded_data)
+
+    print(response)
--- src/utils/alerts_handler_service_utils.py
+++ src/utils/alerts_handler_service_utils.py
@@ -1,4 +1,24 @@
+import requests
+import json
+from configparser import ConfigParser
+
+
 def string_to_list(currencies_list):
     currencies_list = list(eval(currencies_list))
 
     return currencies_list
+
+
+def webhook_post_request(decoded_data):
+    config_file = "../config.ini"
+    config = ConfigParser()
+    config.read(config_file)
+
+    currencies_list = config.get("webhook", "url")
+
+    response = requests.post(
+        currencies_list,
+        data=json.dumps(decoded_data),
+        headers={'Content-Type': 'application/json'})
+
+    return response
